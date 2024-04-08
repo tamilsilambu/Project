af105b6dd21db7588dbe22b1603af86048b52634
tamilsilambu <tamilmech47@gmail.com>
2024-04-08 15:13:14 +0530

    third day

--- src/org/web/Facebook.java
+++ src/org/web/Facebook.java
@@ -1,5 +1,7 @@
 package org.web;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +13,7 @@ public static void main(String[] args) throws InterruptedException {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\tamilarasan\\eclipse-workspace\\New\\Selenium\\driver\\chromedriver.exe");
 	WebDriver driver= new ChromeDriver();
 	driver.get("https://www.facebook.com/");
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	WebElement acc = driver.findElement(By.xpath("//a[text()='Create new account']"));
 	acc.click();
 	Thread.sleep(2000);
